420b0d4ee0b18841e641bc0d8720d05fa2f825fe
albinamurtezaj <110898241+albinamurtezaj@users.noreply.github.com>
2022-12-06 15:31:28 +0100

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+function Footer() {
+    return (
+    <>
+   
+    
+    <div className="layout-footer">
+      <a
+      href="https://www.informatik.hu-berlin.de/de/forschung/gebiete/cses/members/albinamurtezaj"
+      target="_blank"
+      rel="noopener noreferrer"
+      
+    >
+      Humboldt-University of Berlin{' '} |
+      
+      {' '}Albina Murtezaj
+      </a>
+    </div>
+  </>
+  )
+}
+
+export default Footer
+
